cc461400ba5193489c54f5826aaf85845f89e717
Sheviny <2187357@talnet.nl>
2025-03-27 10:07:39 +0100

    docent en vak class

--- app/Models/Docent.php
+++ app/Models/Docent.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Docent extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['naam', 'hobby', 'datum_invoer'];
+
+    public function vakken()
+    {
+        return $this->hasMany(Vak::class);
+    }
 }
--- app/Models/Vak.php
+++ app/Models/Vak.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vak extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['naam', 'beschrijving', 'docent_id'];
+
+    public function docent()
+    {
+        return $this->belongsTo(Docent::class);
+    }
 }
